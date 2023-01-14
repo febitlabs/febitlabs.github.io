d7e1fa437bf6a9493a8edd02333ab7be3d656980
Ashutosh Kumar Singh <sng11asu@gmail.com>
2023-01-14 21:24:40 +0530

    carousel

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,57 @@
+import styles from './styles.module.scss';
+// import {Button, Col, Row} from 'react-bootstrap';
+// // import {Facebook, Linkedin, Instagram} from 'react-bootstrap-icons';
+// const Footer = () => {
+//   return (
+//     <Row className={styles.footer}>
+//       <Col className={styles.columns}>
+//         <h4>About Us</h4>
+//         <p>
+//           Ready to book with us? Have a question about our rooms or services?
+//           We'll be happy to help!
+//         </p>
+//         <h4>Social Media</h4>
+//         <p>
+//           <a
+//             className={styles.socialMediaLink}
+//             href="facebook.com/pages/category/Hotel-Services-Company/Bedoff-113730700369174/"
+//           >
+//             <Facebook className={styles.socialMediaIcon} />
+//           </a>{' '}
+//           <a
+//             className={styles.socialMediaLink}
+//             href="https://www.instagram.com/bedoff_ig/"
+//           >
+//             <Instagram className={styles.socialMediaIcon} />
+//           </a>{' '}
+//           <a className={styles.socialMediaLink} href="">
+//             <Linkedin className={styles.socialMediaIcon} />
+//           </a>
+//         </p>
+//       </Col>
+//       <Col className={styles.columns}>
+//         <h4>Contact Us</h4>
+//         <p>B-17, First Floor, Home Solution Mall, Sakchi, Jamshedpur-831006</p>
+//         <br />
+//         <p>reservations@bedoff.com</p>
+//         <p>+91 8197723127</p>
+//       </Col>
+//       <Col className={styles.columns}>
+//         <h4>Map</h4>
+//         <a
+//           className={styles.Map}
+//           href="https://www.google.com/maps/place/Bedoff+Travels/@22.7783686,86.1782435,17z/data=!3m1!4b1!4m5!3m4!1s0x39f5e383e1ad2ff1:0xdfff220bf7844445!8m2!3d22.7783602!4d86.1804297"
+//           target="_blank"
+//         >
+//           <img src="images\map.svg" className={styles.Map} />
+//         </a>
+//         <p>Front desk hours</p>
+//         <p>9:00 am - 7:00pm</p>
+//         <Button className={styles.bookNow} href="/booking">
+//           BOOK NOW
+//         </Button>
+//       </Col>
+//     </Row>
+//   );
+// };
+// export default Footer;
