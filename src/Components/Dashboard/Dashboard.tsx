6304dec2531248c1c5ea57bfd5e09f63ab93eaf3
a3-athar <a3.athar@gmail.com>
2023-01-17 16:47:30 +0530

    Architecture Stack component and PrimeFlex dependency added.

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,12 +1,14 @@
 import Tabs from '../Navbar/Tabs';
 import FebitCarousel from '../Carousel/Carousel';
 import Footer from '../Footer/Footer';
+import StackComponent from './StackComponent';
 
 const Dashboard = () => {
   return (
     <>
       {/* <Tabs /> */}
       <FebitCarousel />
+      <StackComponent />
       <Footer />
     </>
   );
