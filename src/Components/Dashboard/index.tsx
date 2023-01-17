143d2f543cfd52e54df14536361bdb72e3e120e7
Ashutosh Kumar Singh <sng11asu@gmail.com>
2023-01-17 11:52:45 +0530

    Update index.tsx

--- src/Components/Dashboard/index.tsx
+++ src/Components/Dashboard/index.tsx
@@ -1,7 +1,6 @@
 import Tabs from '../Navbar/Tabs';
 import FebitCarousel from '../Carousel';
 import Footer from '../Footer';
-import {Container} from 'react-bootstrap';
 
 const Dashboard = () => {
   return (
